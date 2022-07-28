1db72a7ffa401e6a1fcba0dd66d3da8f7cae563c
evanbowman <ebowman@bu.edu>
2022-07-28 17:05:59 -0400

    initial commit

---
+++ main.lua
@@ -0,0 +1,20 @@
+
+txtr(0, "overlay/overlay.bmp")
+txtr(2, "tiles/tilesheet.bmp")
+txtr(4, "sprites/spritesheet.bmp")
+
+
+
+print("Hello, World!", 1, 1)
+
+
+while true do
+
+   -- update logic here:
+
+   clear()
+
+   -- graphics updates here:
+
+   display()
+end
---
+++ manifest.lua
@@ -0,0 +1,26 @@
+
+local app = {
+   name = "NewProject",
+
+   tilesets = {
+      "overlay/overlay.bmp",
+      "tiles/tilesheet.bmp",
+   },
+
+   spritesheets = {
+      "sprites/spritesheet.bmp",
+   },
+
+   audio = {
+   },
+
+   scripts = {
+      "main.lua",
+   },
+
+   misc = {
+
+   }
+}
+
+return app
